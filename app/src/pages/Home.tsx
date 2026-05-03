bb839a36b773e847a14a80c311bfb08e1a9f32f3
Huntiverse <philippe_savy@hotmail.com>
2026-05-03 17:07:00 +0400

    Add files via upload

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from 'react'
+import Lenis from '@studio-freight/lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Navigation from '../sections/Navigation'
+import Hero from '../sections/Hero'
+import About from '../sections/About'
+import Work from '../sections/Work'
+import Contact from '../sections/Contact'
+import Footer from '../sections/Footer'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function Home() {
+  const lenisRef = useRef<Lenis | null>(null)
+
+  useEffect(() => {
+    // Initialize Lenis smooth scroll
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+    })
+
+    lenisRef.current = lenis
+
+    // Connect Lenis to GSAP ScrollTrigger
+    lenis.on('scroll', ScrollTrigger.update)
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      lenis.destroy()
+      gsap.ticker.remove(lenis.raf as any)
+    }
+  }, [])
+
+  return (
+    <div className="relative" style={{ background: '#050505' }}>
+      <Navigation />
+      <main>
+        <Hero />
+        <About />
+        <Work />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
